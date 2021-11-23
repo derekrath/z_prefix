b5c416ab7cd5b7a4e0050da49954a28f802d90b4
Derek Rath <lompoc.prosolutions@gmail.com>
2021-11-23 14:40:04 -0800

    bcrypt and removed server volumes

--- Dockerfile
+++ Dockerfile
@@ -13,9 +13,16 @@ EXPOSE 3000
 # //remove for production (dont expose to public)
 # EXPOSE 5432
 
+RUN rm -f node_modules
+
 RUN npm install
 
 # delete on production
 RUN npm install -g nodemon
+
+# RUN apk add --no-cache make gcc g++ python && \
+#   npm install && \
+#   npm rebuild bcrypt --build-from-source && \
+#   apk del make gcc g++ python
 
 CMD npm start
--- server.js
+++ server.js
@@ -7,11 +7,63 @@ const PORT = 3000;
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development']);
 const cors = require('cors');
 const morgan = require('morgan');
+const bcrypt = require('bcrypt');
+
+// salty server stuff
+const saltRounds = 12;
+const { hash, compare } = bcrypt; 
+
+function createNewUser(username, passwordHash){
+    return knex('users')
+        .insert({ username, passwordHash })
+        .then(data=>data);
+}
+
+function getPasswordHashByUser(username){
+    return knex('users')
+        .where({username})
+        .select('passwordHash')
+        .then((data) => data[0].passwordHash)
+}
 
 app.use(morgan());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/login/createUser', (req, res) => {
+    let { body } = req;
+    let { username, passwordRaw } = body;
+
+    hash(passwordRaw, saltRounds)
+        .then((passwordHash) => {
+            console.log('Raw password:', passwordRaw)
+            console.log('Hashed Password', passwordHash)
+            createNewUser(username, passwordHash)
+                .then((data) = res.status(201).json('NEW USER CREATED'))
+                .catch((err) => res.status(500).json(err));
+        })
+        .catch((err) => res.status(500).json(err));
+});
+
+app.post('/login/getUser', (req, res) => {
+    let { body } = req;
+    let { username, passwordRaw } = body;
+
+    getPasswordHashByUser(username)
+        .then((passwordHash) => {
+            console.log('Raw password supplied:', passwordRaw)
+            console.log('Hashed Password form db', passwordHash)
+            compare( passwordRaw, passwordHash)
+                .then((isMatch) => {
+                    if (isMatch) res.status(202).json('PASSWORDS MATCH');
+                    else res.status(401).json('NO PASSWORD MATCH');
+                })
+                .catch((err) => res.status(500).json(err))
+        })
+});
+
+//data demo:
 
 app.get('/', async (req,res) => {
     console.log('Getting from /');
