086df091310c7c83a3cb88b89ed3a3a98ff60c2a
Derek Rath <lompoc.prosolutions@gmail.com>
2023-04-11 22:46:35 -0700

    not fixed yet

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,30 @@
+require("dotenv").config();
+
 // Update with your config settings.
 // const connection = process.env.DB_CONNECTION_STRING;
+// <<use this for dev without ssl
 const connection = {
   connectionString: process.env.DB_CONNECTION_STRING, // IMPORTANT - HEROKU SETS THIS AND CHANGES IT PERIODICALLY
   ssl: {
     rejectUnauthorized: false,
   },
 };
-require("dotenv").config();
+// <<use this for production
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: connection
+    // connection: process.env.DB_CONNECTION_STRING,
     // connection: 'postgres://postgres:docker@localhost/postgres'
   },
 
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      // database: 'my_db',
+      database: 'db',
       user:     'username',
       password: 'password'
     },
@@ -33,19 +38,21 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    client: 'pg',
+    // client: 'postgresql',
+    connection,
+    // connection: {
+    //   // database: 'my_db',
+    //   database: 'db',
+    //   user:     'username',
+    //   password: 'password'
+    // },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
 };
