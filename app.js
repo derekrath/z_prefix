3ddee0be44724171166da8294cb71a2db03a706c
derekrath <89831182+derekrath@users.noreply.github.com>
2023-08-04 10:41:18 -0700

    App successfully deployed on Heroku

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 const knex = require('./db_controllers/dbConnection');
 // const { check } = require('yargs');
 
-console.log(`NODE ENVIRONMENT IN HEROKU : `, process.env.NODE_ENV);
-console.log(`PORT IN HEROKU : `, process.env.PORT);
+// console.log(`NODE ENVIRONMENT IN HEROKU : `, process.env.NODE_ENV);
+// console.log(`PORT IN HEROKU : `, process.env.PORT);
 
 // Get data from ui and update Database
 //create user
